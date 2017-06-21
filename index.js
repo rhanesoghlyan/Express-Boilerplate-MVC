db3a9dc36b5c7dd1e9feb723a27c5517f1c69ced
Gagik <alixanyangagik@gmail.com>
2017-06-21 11:22:27 +0400

    updated config files

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.engine('.html', require('ejs').__express);
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, configs.viewPath, 'post'));
+app.set('views', path.join(__dirname, configs.front.viewPath,'post'));
 
 app.get('/',  (req, res) =>{
     res.render('create', {
