c21dcdd72387164548007674da6dc223106fcd02
Gagik <alixanyangagik@gmail.com>
2017-06-26 16:15:21 +0400

    Installed lodash and winston modules

--- router/handlers/home/index.js
+++ router/handlers/home/index.js
@@ -1,8 +1,9 @@
 const meta = require ('../../../meta');
+const _=require('lodash');
 
 module.exports=function (req,res) {
-    res.render('home',{
+    res.render('home',_.assign({},{
         title:'Welcome at Home :)',
         meta: meta.home
-    })
+    }))
 };
