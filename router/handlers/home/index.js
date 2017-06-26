750b89f2ce7be517aaa4e980c7fdd57f11dbd105
Gagik <alixanyangagik@gmail.com>
2017-06-26 15:29:01 +0400

    added support to send meta data

--- router/handlers/home/index.js
+++ router/handlers/home/index.js
@@ -1,5 +1,8 @@
+const meta = require ('../../../meta');
+
 module.exports=function (req,res) {
     res.render('home',{
-        title:'Welcome at Home :)'
+        title:'Welcome at Home :)',
+        meta: meta.home
     })
 };
