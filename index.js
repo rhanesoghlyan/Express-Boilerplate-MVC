8dc629f928656927f1679794e1f74c5cfccc7472
ArtashMardoyan <artashmardoyan@gmail.com>
2017-06-28 18:04:32 +0400

    integrated i18n-express plugin

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const configs = require('./config/env');
+const i18n= require('i18n-express');
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -9,12 +10,19 @@ app.engine('.html', require('ejs').__express);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, configs.front.viewPath, 'post'));
 
-app.use(cookieParser);
+app.use(cookieParser());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+app.use( i18n({
+    translationsPath: path.join(__dirname, 'i18n', 'locales'),
+    siteLangs: ['en'],
+    browserEnable: false,
+    defaultLang: process.env.LNG || 'en'
+}) );
 
 require(configs.front.router)(app);
 
